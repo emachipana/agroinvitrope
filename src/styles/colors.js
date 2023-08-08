c0ad506a401f360548dd5bd8cb0b9acef07bf6ae
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-08 12:34:54 -0500

    ADD: font

---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+	primary: "#286141",
+	secondary: "#524C4C",
+	gray: {
+		primary: "#140101",
+		secondary: "#212529"
+	}
+}
---
+++ src/styles/fonts.js
@@ -0,0 +1,4 @@
+export const font = {
+  primary: "'Quicksand', sans-serif;",
+  secondary: "'Hind Madurai', sans-serif;"
+}

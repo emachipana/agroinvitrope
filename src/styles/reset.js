3511a59f352bb3ed5b384aab814914933727c958
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-08 12:41:49 -0500

    ADD: reset styles

---
+++ src/styles/reset.js
@@ -0,0 +1,27 @@
+import { css } from "@emotion/react";
+import { colors } from "./colors";
+import { font } from "./font";
+
+export const reset = css`
+  @import url('https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400;500;600;700&family=Quicksand:wght@300;400;500;600;700&display=swap');
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  body {
+    font-family: ${font.primary};
+    color: ${colors.gray.primary};
+    font-size: 1rem;
+  }
+`;

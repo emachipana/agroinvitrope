cfa4e51010f8811ff26cfb2a8c9710c6fa7f16a5
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-10 13:40:03 -0500

    SETUP: routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/home";
+
 function App() {
   return (
-    <div>
-      <h1>Agroinvitro Peru</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/productos" element={<h1>Products page</h1>} />
+        <Route path="/nosotros" element={<h1>About us page</h1>} />
+        <Route path="/contacto" element={<h1>Contact page</h1>} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 

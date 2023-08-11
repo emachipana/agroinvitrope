e683ab4447a827356f66554892456c7021243742
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-11 12:07:07 -0500

    ADD: slider on home page

---
+++ src/pages/home.js
@@ -0,0 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Section, Image } from "./styles";
+import { Autoplay } from "swiper/modules";
+import InfoCard from "../components/InfoCard";
+import "swiper/css";
+
+function HomePage() {
+  return (
+    <>
+      <Section>
+        <InfoCard />
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false
+          }}
+          modules={[Autoplay]}
+        >
+          <SwiperSlide>
+            <Image
+              alt="image"
+              src="assets/complete_2.jpg"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              alt="image"
+              src="assets/complete_4.jpg"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              alt="image"
+              src="assets/tubo_1.jpg"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </Section>
+    </>
+  );
+}
+
+export default HomePage;

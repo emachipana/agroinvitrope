fea0e20a0c4ac6e9b69cacb4c675adce544dd1d4
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-15 17:46:58 -0500

    ADD: banner component

---
+++ src/components/Banner/styles.js
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+import { font } from "../../styles";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  padding: 1rem;
+  position: absolute;
+  width: 100%;
+  z-index: 2;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, .2);
+`;
+
+export const Text = styled.h3`
+  font-size: 2.5rem;
+  font-weight: 900;
+  font-family: ${font.secondary};
+  color: rgb(255, 255, 255);
+  text-align: center;
+  max-width: 40vw;
+
+  @media screen and (max-width: 700px) {
+    font-size: 2rem;
+    width: 100%;
+  }
+
+  @media screen and (max-width: 500px) {
+    font-size: 1.5rem;
+    width: 100%;
+  }
+`;

d5a9edcddabe71b6e3886c63aaa920455a8378f1
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-15 17:50:22 -0500

    ADD: props for font size in button component

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -8,15 +8,15 @@ export const Container = styled.div`
   color: white;
   font-weight: 700;
   cursor: pointer;
-  align-self: start;
+  align-self: ${({ alignment }) => alignment ? alignment : "start"};
   transition: .2s ease-in;
-  font-size: 1rem;
+  font-size: ${({ fontSize }) => fontSize ? fontSize : 1}rem;
 
   &:hover {
     transform: scale(1.06);
   }
 
   @media screen and (max-width: 700px) {
-    font-size: 14px;
+    font-size: ${({ minFontSize }) => minFontSize ? minFontSize : 14}px;
   }
 `;

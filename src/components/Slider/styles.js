c663a4ff135bffda9ca89c492d16ae16048420db
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-14 19:31:22 -0500

    ADD: component slider

---
+++ src/components/Slider/styles.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Section = styled.div`
+  position: relative;
+  display: flex;
+  width: 90%;
+  margin: auto;
+  max-height: 86vh;
+  overflow: hidden;
+  margin-bottom: 4rem;
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+
+  @media screen and (max-width: 700px) {
+    width: 100%;
+    height: 65vh;
+  }
+`;
+
+export const Image = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`;
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,23 +1,67 @@
 import styled from "@emotion/styled";
+import { colors, font } from "../styles";
 
-export const Section = styled.div`
-  position: relative;
-  display: flex;
-  width: 90%;
+export const Line = styled.hr`
   margin: auto;
-  max-height: 86vh;
-  overflow: hidden;
+  width: 80%;
+  background-color: ${colors.secondary};
+  border: 1.5px solid ${colors.secondary};
+`;
 
-  @media screen and (max-width: 700px) {
+export const Container = styled.div`
+  margin: auto;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 1rem;
+`;
+
+export const Section = styled.section`
+  width: 75%;
+  padding: 1.5rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-direction: ${({ direction }) => direction};
+  ${({ direction }) => direction === "column"
+    ?
+      "width: 20vw;"
+    :
+      ""
+  }
+  gap: ${({ direction }) => direction === "column" ? 1 : 2}rem;
+
+  @media screen and (max-width: 950px) {
     width: 100%;
-    height: 65vh;
   }
 `;
 
 export const Image = styled.img`
-  margin: auto;
-  display: block;
   object-fit: cover;
-  width: 100%;
+  border-radius: 1rem;
   height: 100%;
+  width: 280px;
+  cursor: pointer;
+  transition: .3s ease-in;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 2.5rem;
+  font-family: ${font.secondary};
+  color: ${colors.primary};
+  font-weight: 900;
+`;
+
+export const Text = styled.p`
+  text-align: start;
+  font-size: 1rem;
+  font-weight: 700;
+  color: ${colors.gray.secondary};
 `;

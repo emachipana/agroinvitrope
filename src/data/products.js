c290eb936a43d3d28ca1290b24a2f3f5ebb8890f
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-17 13:18:45 -0500

    ADD: experimental data for products

---
+++ src/data/products.js
@@ -0,0 +1,14 @@
+export const PRODUCTS = [
+  {
+    name: "Medio de Cultivo",
+    description: "El medio en forma de polvo incluye los macronutrientes, micronutrientes y vitaminas según lo descrito por Murashige & Skoog",
+    src: "ms.png",
+    category: "insumo"
+  },
+  {
+    name: "Bandeja de cultivo",
+    description: "El medio en forma de polvo incluye los macronutrientes, micronutrientes y vitaminas según lo descrito por Murashige & Skoog",
+    src: "bandeja.png",
+    category: "material"
+  }
+];

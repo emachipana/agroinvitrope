a1ee637ce66b429884a99b4db6cc958678410ddb
Enmanuel Chipana <emachipana0102@gmail.com>
2023-08-28 10:37:39 -0500

    ADD: services data

---
+++ src/data/services.js
@@ -0,0 +1,61 @@
+export const SERVICES = [
+  {
+    name: "Diseño",
+    img: {
+      src: "invernadero.gif",
+      alt: "invernadero-photo"
+    },
+    details: [
+      {
+        title: "Invernadero",
+        list: [
+          "Diseño especializado",
+          "Con las dimensiones adecuadas",
+          "Y requisitos mínimos"
+        ]
+      },
+      {
+        title: "Laboratorio",
+        list: [
+          "Diseño especializado",
+          "Con los requisitos requeridos",
+          "Areas y dimensiones adecuadas"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Cursos y Talleres",
+    img: {
+      src: "complete_3.jpg",
+      alt: "courses-photo"
+    },
+    details: [
+      {
+        title: "Micropropagación",
+        list: [
+          "Preparación de reactivos",
+          "Introducción de plantas madres",
+          "Equipos especializados"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Asesoramiento",
+    img: {
+      src: "tuberculillo.jpg",
+      alt: "potato-photo"
+    },
+    details: [
+      {
+        title: "Producción de tuberculillos",
+        list: [
+          "Asesoramiento especializado",
+          "Vasta experiencia con tuberculillos",
+          "Con semillas de alta calidad"
+        ]
+      }
+    ]
+  }
+];

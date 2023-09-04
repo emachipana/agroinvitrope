dfc854b303636c50e6a527a12fe2383ada531bc9
Enmanuel Chipana <emachipana0102@gmail.com>
2023-09-04 12:32:15 -0500

    ADD: render loader by time

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import ServicesPage from "./pages/Services";
 import ContactPage from "./pages/Contact";
 import AboutPage from "./pages/About";
 import WhatsappButton from "./components/WspButton";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, [isLoading]);
+
   return (
     <>
+      { !isLoading || <Loader /> }
       <WhatsappButton />
-      <Navbar />
+      <Navbar 
+        setIsLoading={setIsLoading}
+      />
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="/productos" element={<ProductsPage />} />
